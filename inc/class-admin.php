2be615587bcad0b5249c6524487444a3ba90c7af
Shramee Srivastav <shramee.srivastav@gmail.com>
2018-08-31 16:45:19 +0530

    Addressing #91

--- inc/class-admin.php
+++ inc/class-admin.php
@@ -66,7 +66,9 @@ class Caxton_Admin {
 
 //		wp_enqueue_script( "$token-components", $url . 'assets/caxton-components.build.js', array( 'wp-blocks' ) );
 		wp_enqueue_script( $token, $url . 'assets/caxton.js', array( 'wp-blocks' ) );
-		wp_enqueue_script( "$token-block", $url . 'assets/caxton-block.js', array( $token ) );
+
+		// Don't include caxton block yet
+		//		wp_enqueue_script( "$token-block", $url . 'assets/caxton-block.js', array( $token ) );
 		wp_enqueue_script( "$token-blocks", $url . 'assets/block.js', array( $token ) );
 		wp_enqueue_style( "$token-blocks", $url . 'assets/block.css' );
 		wp_enqueue_script( $token . '-js', $url . 'assets/caxton-utils.js', array( 'jquery' ) );
